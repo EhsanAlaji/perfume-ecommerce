f3cecfbf9dfe1b7b4d3bc59965ea3c015e081535
EhsanAlaji <ehsan.alaji175@gmail.com>
2026-02-23 01:41:45 +0200

    new change

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,4 +1,3 @@
-
 import Image from 'next/image';
 import getCategories from '@/api/getcategories';
 import { Category } from '@/type/category.type';
@@ -11,40 +10,40 @@ import {
 
 export default async function Categories() {
 
-  const data: Category[] = await getCategories();
+  const data: Category[] = await getCategories() || []; // fallback Array فارغ
+
+  if (!Array.isArray(data) || data.length === 0) {
+    return <div className='text-center py-10'>No categories available</div>;
+  }
 
   return (
     <div className='container mx-auto my-4 md:w-[80%] w-full'>
       <div className="flex flex-wrap">
 
-        {data?.map((category: Category) => (
+        {data.map((category: Category) => (
           <div
             key={category._id}
             className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-2"
           >
-            
-              <div className='px-2 bg-white shadow-md p-3 rounded-3xl'>
+            <div className='px-2 bg-white shadow-md p-3 rounded-3xl'>
+              <CardContent>
+                <p className='font-bold line-clamp-1'>
+                  {category.name}
+                </p>
+              </CardContent>
 
-                <CardContent>
-                  <p className='font-bold line-clamp-1'>
-                    {category.name}
-                  </p>
-                </CardContent>
-
-                <CardHeader>
-                  <CardTitle>
-                    <Image
-                      width={200}
-                      height={200}
-                      className='w-full'
-                      src={category.image}
-                      alt={category.name}
-                    />
-                  </CardTitle>
-                </CardHeader>
-
-              </div>
-            
+              <CardHeader>
+                <CardTitle>
+                  <Image
+                    width={200}
+                    height={200}
+                    className='w-full'
+                    src={category.image}
+                    alt={category.name}
+                  />
+                </CardTitle>
+              </CardHeader>
+            </div>
           </div>
         ))}
 
