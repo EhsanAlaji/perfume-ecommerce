2018e8487abd7c1db80cd94118deb40bb45a9fe2
EhsanAlaji <ehsan.alaji175@gmail.com>
2026-02-23 00:54:08 +0200

    new change

--- src/app/_components/category slider/category-slider.tsx
+++ src/app/_components/category slider/category-slider.tsx
@@ -1,18 +1,16 @@
-import getAllCategories from '@/api/getAllCategories';
-import MySlider from '@/components/myslider/myslider';
-import { Category } from '@/type/category.type';
-import { get } from 'http'
-import React from 'react'
+import getAllCategories from "@/api/getAllCategories";
+import MySlider from "@/components/myslider/myslider";
+import { Category } from "@/type/category.type";
+import React from "react";
 
-export default async function CategorySlider({categories}:{categories:Category[]}) {
-    const data = await getAllCategories();
+export default async function CategorySlider() {
+  const data = await getAllCategories();
 
-    const dataimage=data.map((ca:Category)=>ca.image);
-  return<>
-  <div className=' w-[80%] mx-auto mt-5 mb-5'>
-<MySlider imgList={dataimage}  slidesPerView={7} />
-  </div>
-  
-  
-  </>
+  const dataimage = data.data.map((ca: Category) => ca.image);
+
+  return (
+    <div className="w-[80%] mx-auto mt-5 mb-5">
+      <MySlider imgList={dataimage} slidesPerView={7} />
+    </div>
+  );
 }
