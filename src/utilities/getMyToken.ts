3901832ea1605921bea0651db120bff44e1d8485
EhsanAlaji <ehsan.alaji175@gmail.com>
2026-02-24 00:42:10 +0200

    new change

--- src/utilities/getMyToken.ts
+++ src/utilities/getMyToken.ts
@@ -1,23 +1,41 @@
-"use server"
-import { decode } from "next-auth/jwt";
-import { cookies } from "next/headers";
+// "use server"
+// import { decode } from "next-auth/jwt";
+// import { cookies } from "next/headers";
 
 
-export async function getMyToken(){
+// export async function getMyToken(){
 
-    const decodeToken =(await cookies()).get("next-auth.session-token")?.value ||
-    (await cookies()).get("__Secure-next-auth.session-token")?.value
+//     const decodeToken =(await cookies()).get("next-auth.session-token")?.value ||
+//     (await cookies()).get("__Secure-next-auth.session-token")?.value
 
 
-        if(!decodeToken) return null;
+//         if(!decodeToken) return null;
 
-       const token = await decode({
-  token: decodeToken,
-  secret: process.env.NEXTAUTH_SECRET!,
-});
+//        const token = await decode({
+//   token: decodeToken,
+//   secret: process.env.NEXTAUTH_SECRET!,
+// });
         
 
 
-       console.log( token?.token)
-    return token?.token;
+//        console.log( token?.token)
+//     return token?.token;
+// }
+
+"use server"
+
+import { getToken } from "next-auth/jwt";
+import { headers } from "next/headers";
+
+export async function getMyToken() {
+  const token = await getToken({
+    req: {
+      headers: {
+        cookie: headers().get("cookie") ?? "",
+      },
+    } as any,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  return token?.token ?? null;
 }
